0a2ecdcad303112860a1c2a8853c13cdc3e26c37
James Miller <jamesstewartmiller@gmail.com>
2025-12-19 16:41:42 +0000

    played with debug_toolbar before realising there were no body tags in the html

--- demo/demo/urls.py
+++ demo/demo/urls.py
@@ -13,13 +13,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('__debug__/', include('debug_toolbar.urls')),
     path('', include('djf_surveys.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
     path('tinymce/', include('tinymce.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += [
+        path("__debug__/", include("debug_toolbar.urls")),
+    ]
