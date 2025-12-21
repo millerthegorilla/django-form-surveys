09247b097dba4688213c5e6e00aecdcaf58aa22c
James Miller <jamesstewartmiller@gmail.com>
2025-12-21 22:39:53 +0000

    removing

--- process_css.py
+++ process_css.py
@@ -191,6 +191,7 @@ if __name__ == "__main__":
             prefix_css_classes_in_html(css_file, [html_path], prefix, action)
         elif os.path.isdir(html_path):
             html_files = find_html_files(html_path)
+            breakpoint()
             prefix_css_classes_in_html(css_file, html_files, prefix, action)
         else:
             print(f"{html_path} does not exist or is not a regular file/directory.")
