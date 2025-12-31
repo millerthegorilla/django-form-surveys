44ef5f9427f2aee803cd0625e9551baca65e368f
James Miller <jamesstewartmiller@gmail.com>
2025-12-31 15:55:27 +0000

    add null=true to survey and surveyselection owner field

--- djf_surveys/models.py
+++ djf_surveys/models.py
@@ -119,7 +119,7 @@ class Survey(BaseModel):
                                         help_text=_("If True a link to the survey will be " \
                                         "listed in the user profile bookmark " \
                                         "list."))
-    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True)
 
     class Meta:
         verbose_name = _("survey")
@@ -308,7 +308,7 @@ class SurveySelection(BaseModel):
                                         help_text=_("If True a link to the survey selection will be " \
                                         "listed in the user profile bookmark " \
                                         "list."))
-    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True)
 
     class Meta:
         constraints = [
