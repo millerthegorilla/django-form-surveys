36e6b3053985b69e146a2a77ed263643c3c0efc4
James Miller <jamesstewartmiller@gmail.com>
2026-01-07 13:28:01 +0000

    made sure the surveyselection zooms to fit the screen

---
+++ djf_surveys/static/djf_surveys/js/survey_selection_fullscreen.js
@@ -0,0 +1,16 @@
+(function(){
+   const content = document.getElementById('survey-content');
+   const currentHeight = content.offsetHeight;
+   const currentWidth = content.offsetWidth;
+   var multiplierH = 0
+   var multiplierW = 0
+   if (currentHeight < screen.height)
+   {
+       multiplierH = currentHeight / screen.height;
+   }
+   else
+   {
+       multiplierH = screen.height / currentHeight;
+   }
+   content.style.zoom = multiplierH - 0.1
+})();
