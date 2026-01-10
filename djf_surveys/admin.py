aff28eb2874ef282964c103e23dc2052216b7092
James Miller <jamesstewartmiller@gmail.com>
2026-01-10 17:48:36 +0000

    stripped out user admin stuff

--- djf_surveys/admin.py
+++ djf_surveys/admin.py
@@ -1,17 +1,7 @@
 from django.contrib import admin
-from django.contrib.admin.exceptions import NotRegistered
-from django.contrib.auth import get_user_model
-from django.contrib.auth.admin import UserAdmin as AuthUserAdmin
-from django.db.models import Case, When
-from django.db.utils import IntegrityError
-from django.shortcuts import redirect
-from django.urls import path
-from django.views.generic.list import ListView
-from faker import Faker
 
 from .models import Answer, Question, Survey, TermsValidators, UserAnswer
 
-User = get_user_model()
 
 
 class AdminQuestion(admin.ModelAdmin):
@@ -46,66 +36,6 @@ class AdminSurvey(admin.ModelAdmin):
 class AdminTermsValidator(admin.ModelAdmin):
     list_display = ("question", "terms")
 
-
-class UserAdmin(AuthUserAdmin):
-    change_list_template = "djf_surveys/admin/change_list.html"
-    actions = ["user_printview"]
-
-    # def get_changeform_initial_data(self, request):
-    #     return {'dave':'dave'}
-
-    # def __init__(self, model, admin_site):
-    #     breakpoint()
-    #     pass
-
-    def user_printview(self, request, queryset):
-        return redirect(
-            "admin:show_added_users", str(list(queryset.values_list("id", flat=True)))
-        )
-
-    def add_10_random_users(self, request):
-        fake = Faker()
-        ids = []
-        for index in range(10):
-            user = User()
-            user.username = fake.passport_number()
-            user.password = fake.password()
-            try:
-                user.save()
-                ids.append(user.id)
-            except IntegrityError:
-                index -= 1
-                continue
-        return redirect("admin:show_added_users", str(ids))
-
-    def get_urls(self):
-        urls = super(AuthUserAdmin, self).get_urls()
-        my_urls = [
-            path("addrand/", self.add_10_random_users),
-            path(
-                "show_added_users/<str:added_users>/",
-                AddedUser.as_view(),
-                name="show_added_users",
-            ),
-        ]
-        return my_urls + urls
-
-
-class AddedUser(ListView):
-    template_name = "djf_surveys/admin/show_added_users.html"
-
-    def get_queryset(self):
-        pk_list = [int(id) for id in self.kwargs["added_users"][1:-1].split(",")]
-        preserved = Case(*[When(pk=pk, then=pos) for pos, pk in enumerate(pk_list)])
-        queryset = User.objects.filter(pk__in=pk_list).order_by(preserved)
-        return queryset
-
-
-try:
-    admin.site.unregister(User)
-    admin.site.register(User, UserAdmin)
-except NotRegistered:
-    pass
 
 admin.site.register(Survey, AdminSurvey)
 admin.site.register(Question, AdminQuestion)
